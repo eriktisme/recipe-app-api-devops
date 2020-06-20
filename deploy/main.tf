20fa7718cf29324f3bf40b9839e503e40150c6d1
Erik van Dam <erik.vandam@trivago.com>
2020-06-20 22:53:57 +0200

    Added terraform for creating bastion instance

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "s3" {
-    bucket = "recipe-app-api-devops-tfstate-erik.vandam"
-    key = "recipe-app.tfstate"
-    region = "eu-west-1"
-    encrypt = true
+    bucket         = "recipe-app-api-devops-tfstate-erik.vandam"
+    key            = "recipe-app.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
     dynamodb_table = "recipe-app-api-devops-tfstate-lock"
   }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1"
   version = "~> 2.54.0"
 }
