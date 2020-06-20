48bafdc607ca3a45fa305b250e24f8c18abbca69
Erik van Dam <erik.vandam@trivago.com>
2020-06-20 18:25:37 +0200

    Added docker-compose config for running terraform locally

---
+++ deploy/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "recipe-app-api-devops-tfstate-erik.vandam"
+    key = "recipe-app.tfstate"
+    region = "eu-west-1"
+    encrypt = true
+    dynamodb_table = "recipe-app-api-devops-tfstate-lock"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  version = "~> 2.54.0"
+}
