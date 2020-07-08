c73b67973f05932e97b7bc13da2d9860e368f084
Erik van Dam <erik.vandam@trivago.com>
2020-07-08 19:43:01 +0200

    Added private subnets

--- deploy/network.tf
+++ deploy/network.tf
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
-# Public Subnets - Inbound/Outbound Internal Access #
+# Public Subnets - Inbound/Outbound Internet Access #
 resource "aws_subnet" "public_a" {
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
@@ -119,4 +119,68 @@ resource "aws_nat_gateway" "public_b" {
     local.common_tags,
     map("Name", "${local.prefix}-public-b")
   )
+}
+
+# Private Subnets - Outbound Internet Access Only
+
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_out" {
+  route_table_id         = aws_route_table.private_a.id
+  nat_gateway_id         = aws_nat_gateway.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.1.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_out" {
+  route_table_id         = aws_route_table.private_b.id
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
 }
