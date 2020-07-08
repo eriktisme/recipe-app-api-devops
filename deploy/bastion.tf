d475d373d716953a3f7966310912bfca228cd0fb
Erik van Dam <erik.vandam@trivago.com>
2020-07-08 21:47:31 +0200

    Updated bastion security groups

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -32,8 +32,49 @@ resource "aws_instance" "bastion" {
   key_name             = var.bastion_key_name
   subnet_id            = aws_subnet.public_a.id
 
+  vpc_security_group_ids = [
+    aws_security_group.bastion.id
+  ]
+
   tags = merge(
     local.common_tags,
     map("Name", "${local.prefix}-bastion")
   )
+}
+
+resource "aws_security_group" "bastion" {
+  description = "Control bastion inbound and outbound access"
+  name        = "${local.prefix}-bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"] # If you have a static IP address, you could specify it here for security reasons.
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol  = "tcp"
+    from_port = 5432
+    to_port   = 5432
+    cidr_blocks = [
+      aws_subnet.private_a.cidr_block,
+      aws_subnet.private_b.cidr_block
+    ]
+  }
 }
